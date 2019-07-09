e5ff27a830d45bed6c1ee4d44edeec35b7db44e4
Richie Jarvis <richie@helkit.com>
2019-07-09 04:45:17 +0100

    long night...

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
-#         speedcam2es.py Variable Configuration Settings
+# speedcam2es.py Variable Configuration Settings
 version = "0.1.4"
 retry_count = 2
 username = "grantham_close_1"
@@ -7,11 +7,7 @@ elasticsearch_url = "https://e5fc3b57281449cf9b43aea35fb519fc.europe-west1.gcp.c
 timezone = "Europe/London"
 db_path = "/home/pi/speedcam2es/data/speed_cam.db"
 db_table = "speed"
-report_query = ("""select * from speed order by idx""")
-#report_query = ("""select * from speed order by idx desc""")
-report_query = ("""select * from speed order by idx limit 2""")
-#report_query = ("""select * from speed where log_hour = strftime("%H",time("now","+01:00")) and log_date = strftime("%Y%m%d",date("now")) order by idx desc""")
-#report_query = ("""select * from speed where log_date = strftime("%Y%m%d",date("now")) order by idx desc""")
+report_query = ('''select * from speed''')
 l2r_direction = "Southbound"
 r2l_direction = "Northbound"
 lat = 50.937186
--- speedcam2es.py
+++ speedcam2es.py
@@ -20,58 +20,84 @@ prog_name = os.path.basename(__file__)
 # Check for variable file to import and error out if not found.
 config_file_path = os.path.join(base_dir, "config.py")
 if not os.path.exists(config_file_path):
-  print("ERROR : Someone nicked my config.py file - File Not Found %s" % config_file_path)
-  config_url = "https://raw.githubusercontent.com/richiejarvis/speedcam2es/master/config.py"
-  print("INFO  : Auto-restore of config.py file from %s" % config_url)
-  try:
-    wgetfile = urllib2.urlopen(config_url)
-  except:
-    print("ERROR : D'oh Download of config.py Failed")
-    print("        %s Exiting Due to Error" % version)
-    sys.exit(1)
-  f = open("config.py", "wb")
-  f.write(wgetfile.read())
-  f.close()
+ print("ERROR : Someone nicked my config.py file - File Not Found %s" % config_file_path)
+ config_url = "https://raw.githubusercontent.com/richiejarvis/speedcam2es/master/config.py"
+ print("INFO  : Auto-restore of config.py file from %s" % config_url)
+ try:
+   wgetfile = urllib2.urlopen(config_url)
+ except:
+     print("ERROR : Doh Download of config.py Failed")
+     print("        %s Exiting Due to Error" % version)
+     sys.exit(1) 
+  
+ f = open("config.py", "wb")
+ f.write(wgetfile.read())
+ f.close()
+ # Now that variables are imported from config.py Setup Logging
+ if loggingToFile:
+   logging.basicConfig(level=logging.DEBUG,
+   format='%(asctime)s %(levelname)-8s %(funcName)-10s %(message)s',
+     datefmt='%Y-%m-%d %H:%M:%S',
+     filename=logFilePath,
+     filemode='w')
+ elif verbose:
+   logging.basicConfig(level=logging.DEBUG,
+   format='%(asctime)s %(levelname)-8s %(funcName)-10s %(message)s',
+   datefmt='%Y-%m-%d %H:%M:%S')
+ else:
+   logging.basicConfig(level=logging.CRITICAL,
+   format='%(asctime)s %(levelname)-8s %(funcName)-10s %(message)s',
+   datefmt='%Y-%m-%d %H:%M:%S')
+from search_config import search_dest_path
 
 def Main():
+  retry_count = 2
   print("Author: My attempt at an Elasticsearch ingestion engine")
   print("Version: %s" % (version))
   print("Author: Richie Jarvis")
   print("Date: 2019-07-01")
   print("GitHub: https://github.com/richiejarvis/speedcam2es")
   print("Description: Convert speed-camera.py sqlite3 db to Elasticsearch Document.  With thanks to speed-camera.py written by Claude Pageau:  https://github.com/pageauc/speed-camera")
-  while retry_count >= retry:
-    connection = sqlite3.connect(db_path)
-    connection.row_factory = sqlite3.Row
-    cursor = connection.cursor()
-    cursor.execute(report_query)
-    row_count = 0
-    record = ""
-    print("Retries left: %s" % retry_count)
-    while True:
+  print("DEBUG: Retries left: %s" % retry_count)
+ # while retry_count <= retry:
+  connection = sqlite3.connect(db_path)
+  connection.row_factory = sqlite3.Row
+  cursor = connection.cursor()
+  cursor.execute(report_query)
+  row_count = 0
+  record = ""
+  speed = ""
+  print("DEBUG: row_count %s" % str(row_count))
+  while row_count <= 10:
       row_count += 1
+      retry_count += 1
       row = cursor.fetchone()
+      print("DEBUG: row: %s" % str(row))
       if row is None: 
         break
       direction = row["direction"]
       timestamp = make_date(row["idx"])
       speed = row["ave_speed"]
-  # DB scrape done... mv alng
-  unique_hash = hashlib.sha1(str(speed)).hexdigest()
-  print("DEBUG: speed: %s hash: %s" % (speed, unique_hash))
-  if direction == "L2R": 
-    direction = r2l_direction 
-  else:
-    direction = l2r_direction 
-  # Got everything we need - print it
-  record = { '@timestamp' : timestamp, 'speed' : speed, 'direction' : direction, 'source' : username, 'lat': lat, 'lng': lng }
-  url = str(elasticsearch_url + "/record/")
-  status == 1
-  while status == 0:
-    status = es_post(url,record,speed,retry)
-  cursor.close()
+      # print(".")
+      unique_hash = hashlib.sha1(str(speed)).hexdigest()
+      # DB scrape done... mv alng
+      print("DEBUG: speed: %s hash: %s" % (speed, unique_hash))
+      if direction == "L2R": 
+        direction = r2l_direction 
+      else:
+        direction = l2r_direction 
+      # Got everything we need - print it
+      record = { '@timestamp' : timestamp, 'speed' : speed, 'direction' : direction, 'source' : username, 'lat': lat, 'lng': lng }
+      # print("DEBUG: record: %s" % record)
+      url = str(elasticsearch_url + "/record/")
+      status = 1
+      while status == 0:
+        status = es_post(url,record,speed,retry)
+        status = es_post(url,record,speed,retry)
+      cursor.close()
   connection.close
-  print("voila")
+print(".")
+print("DEBUG: voila")
 
 def make_date(string):
     # 0123456789012345
@@ -85,14 +111,22 @@ def make_date(string):
     return string
 
 def es_post(es,record,speed,retry):
- unique_hash = hashlib.sha1(es + str(retry_count)).hexdigest()
- try:
-  resp = requests.post(es,auth=(username,password),verify=False,json=record)
-  print(" Rtry: " + str(retry) + " spd: " + str(speed) + " " + unique_hash )
-  retry =+ 1
- except:
-  return 0
-  pass
- return 1
+  waste_some_time(100)
+  unique_hash = hashlib.sha1(record + str(retry)).hexdigest()
+  try:
+    resp = requests.post(es,auth=(username,password),verify=False,json=record)
+    print(" DEBUG: Rtry: " + str(retry) + " spd: " + str(speed) + " " + unique_hash )
+    retry =+ 1
+  except:
+    return 0
+    pass
+  return 1
+
+def waste_some_time(count2):
+    bob = 0
+    while bob < count2:
+      print(".")
+      bob += 1
+
 if __name__ == "__main__":
-    Main()
+ Main()
