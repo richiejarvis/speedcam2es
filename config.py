ee1574a96dfcb8fe9bc02b32da208b4235f156d7
richiejarvis-elastic <richard.jarvis@elastic.co>
2019-07-11 12:48:29 +0100

    version change

--- config.py
+++ config.py
@@ -1,11 +1,11 @@
 # ---------------- User Configuration Settings for speedcam2es.py ---------------------------------
-#         Ver 0.0.1 speedcam2es.py Variable Configuration Settings
-version = "0.1.2"
+#          speedcam2es.py Variable Configuration Settings
+version = "0.3.0"
 username = "uk_chailey_grantham_3"
 password = "MB4brekky"
 elasticsearch_url = "https://jd:9243/chailey-"
 timezone = "Europe/London"
-db_path = '/home/pi/speed-camera/data/speed_cam.db'
+db_path = './data/speed_cam.db'
 db_table = 'speed'
 report_query = ('''select * from speed order by idx desc limit 100''')
 #report_query = ('''select * from speed where log_hour > strftime('%H',time('now')) and log_date = strftime('%Y%m%d',date('now')) order by idx desc''')
