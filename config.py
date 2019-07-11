3c8092c2b5f7053036d28005d350d23e29db2712
Richie Jarvis <richie@helkit.com>
2019-07-11 21:41:51 +0100

    saving default config

--- config.py
+++ config.py
@@ -1,19 +1,19 @@
 # ---------------- User Configuration Settings for speedcam2es.py ---------------------------------
 #         Ver 0.0.1 speedcam2es.py Variable Configuration Settings
-version = "0.1.2"
+version = "0.1.6"
 username = "uk_chailey_grantham_3"
-password = "MB4brekky"
-elasticsearch_url = "https://jd:9243/chailey-"
+password = "password"
+elasticsearch_url = "https://host:port/index"
 timezone = "Europe/London"
 db_path = '/home/pi/speed-camera/data/speed_cam.db'
 db_table = 'speed'
-report_query = ('''select * from speed order by idx desc limit 100''')
+report_query = ('''select * from speed order by idx desc limit 1''')
 #report_query = ('''select * from speed where log_hour > strftime('%H',time('now')) and log_date = strftime('%Y%m%d',date('now')) order by idx desc''')
 #report_query = ('''select * from speed where log_date = "20190629" order by idx desc''')
 #report_query = ('''select * from speed where log_date = "20190629" order by idx ''')
 #report_query = ('''select * from speed where log_date = "20190630" order by idx ''')
-lat = "50.930"
-lng = "-0.02"
+lat = "50.0"
+lng = "-0.00"
 speed_unit = "mph"
-ssl_verify = False
+ssl_verify = True
 debug = True
